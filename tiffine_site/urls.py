91fc32a94a78ea271665a32d116fbf9304518f2c
vikasOzark <vikask99588@gmail.com>
2022-02-27 14:09:23 +0530

    update in forget password function

--- tiffine_site/urls.py
+++ tiffine_site/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -13,5 +14,25 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('logout_user/', logout_user, name='logout_user'),
     path('add-favorite/', add_favorite, name='add-favorite'),
-    path('user-profile/', user_profile, name='user-profile')
+    path('user-profile/', user_profile, name='user-profile'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset_form.html'
+    ), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+         template_name='password_reset_done.html'
+         ), name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        #     template_name='password_reset_confirm.html'
+    ),
+        name='password_reset_confirm'),
+
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+         template_name='password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
