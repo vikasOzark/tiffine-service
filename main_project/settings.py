699fa173b1ff4f65b8f7148763074244274d703c
vikas <vikask99588@gmail.com>
2022-08-15 13:21:24 +0530

    updates v1.5

--- main_project/settings.py
+++ main_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['https://tiffine.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 #test change
 CSRF_TRUSTED_ORIGINS = [
